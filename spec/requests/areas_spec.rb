f474ecd5ef00e61a4657c4bc6cbc58d8a7fb946e
Széll Dávid <szell.david@outlook.com>
2018-04-20 01:54:01 +0200

    Area test and controller fix

--- spec/requests/areas_spec.rb
+++ spec/requests/areas_spec.rb
@@ -6,6 +6,20 @@ RSpec.describe 'Areas API', type: :request do
   let(:floor_id) { floor.id }
   let(:id) { areas.first.id }
 
+  # Test suite for GET /areas
+  describe 'GET /areas' do
+    before { get "/areas" }
+
+    it 'returns status code 200' do
+      expect(response).to have_http_status(200)
+    end
+
+    it 'returns areas' do
+      expect(json).not_to be_empty
+      expect(json.size).to eq(10)
+    end   
+  end
+
   # Test suite for GET /floors/:floor_id/areas
   describe 'GET /floors/:floor_id/areas' do
     before { get "/floors/#{floor_id}/areas" }
@@ -37,6 +51,33 @@ RSpec.describe 'Areas API', type: :request do
   # Test suite for GET /floors/:floor_id/areas/:id
   describe 'GET /floors/:floor_id/areas/:id' do
     before { get "/floors/#{floor_id}/areas/#{id}" }
+  
+    context 'when area exists' do
+      it 'returns status code 200' do
+        expect(response).to have_http_status(200)
+      end
+  
+      it 'returns the area' do
+        expect(json['id']).to eq(id)
+      end
+    end
+  
+    context 'when area does not exist' do
+      let(:id) { 0 }
+  
+      it 'returns status code 404' do
+        expect(response).to have_http_status(404)
+      end
+  
+      it 'returns a not found message' do
+        expect(response.body).to match(/Couldn't find Area/)
+      end
+    end
+  end
+
+  # Test suite for GET /areas/:id
+  describe 'GET /areas/:id' do
+    before { get "/areas/#{id}" }
   
     context 'when area exists' do
       it 'returns status code 200' do
